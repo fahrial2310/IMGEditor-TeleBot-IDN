c3602f81f8c35fc64ea1d6c5e07bc4a05bb9822a
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2021-09-20 19:49:27 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,8 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     
     BOT_NAME = os.environ.get("BOT_NAME", "")
+    
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
 
     APP_ID = int(os.environ.get("APP_ID", 12345))
 
