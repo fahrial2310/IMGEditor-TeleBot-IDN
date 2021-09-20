aab6856491ad0a8f5445876816094425b9e860bb
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2021-09-20 19:08:53 +0800

    Update script.py

--- script.py
+++ script.py
@@ -1,10 +1,11 @@
 class script(object):
+from config import config
 
     START_MSG = """ <b>Halo {}
 
 saya adalah bot editor yang dapat edit gambar yanganda inginkan
 klik help untuk bantuan....</b>"""
-    HELP_MSG = """Follow These Steps for help..
+    HELP_MSG = """ikuti step ini untuk bantuan..
 
 <code>1️> Kirim Saya Gambar Apa Saja Untuk Diedit..</code>
 
@@ -13,12 +14,10 @@ klik help untuk bantuan....</b>"""
 <code>3️> Gambar Hasil Editan Anda Akan Diunggah </code>
 
 ☠️ @Alvin_junior"""
-    ABOUT_MSG = """☠️ <b>nama saya adalah:</b> :</b> <a href='t.me/AlvinImageEditor'>Alvin Image Editor ☠️</a>
+    ABOUT_MSG = """☠️ <b>nama saya adalah:</b> :</b> <a href='t.me/{BOT_USERNAME}'>{BOT_NAME} ☠️</a>
 ☠️ <b>Creator :</b> <a href='t.me/Alvin_junior'>Muhammad Fahri</a>
 
-☠️ <b>Support Group :</b> <a href='t.me/Alvin_image_editor_group'>Alvin Support</a>
+☠️ <b>Support Group :</b> <a href='t.me/{SUPPORT_GRP}'>{SUPPORT_NAME}</a>
 
-☠️ <b>Updates Channel :</b> <a href='t.me/Alvin_image_editor'>Alvin Updates</a>
-
-☠️ <b>off topic Group :</b> <a href ='t.me/Alvin_image_editor_helpers'>Off Topic</a>
+☠️ <b>Updates Channel :</b> <a href='t.me/{UPDATES_CH}r'>{UPDATES_NAME}</a>
 """
