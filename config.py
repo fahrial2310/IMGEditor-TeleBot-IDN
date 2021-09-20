7192fbe78c75e7605b7c23e78b87997cd56a4e65
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2021-09-20 18:32:30 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,9 @@ class Config(object):
     APP_ID = int(os.environ.get("APP_ID", 12345))
 
     API_HASH = os.environ.get("API_HASH", "")
-
+    
+    SUPPORT_GRP = os.environ.get("SUPPORT_GRP", "")
+    
+    UPDATES_CH = os.environ.get("UPDATES_CH", "")
     # Get this api from https://www.remove.bg/b/background-removal-api
     RemoveBG_API = os.environ.get("RemoveBG_API", "")
