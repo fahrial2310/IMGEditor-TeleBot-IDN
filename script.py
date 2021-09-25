e8442ee75993a80a55433b43121cf6dc8246d69e
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2021-09-25 12:55:27 +0800

    Update script.py

--- script.py
+++ script.py
@@ -17,9 +17,9 @@ klik help untuk bantuan....</b>"""
     ABOUT_MSG = f"""☠️ <b>nama saya adalah:</b> :</b> <a href='t.me/{BOT_USERNAME}'>{BOT_NAME} ☠️</a>
 ☠️ <b>Creator :</b> <a href='t.me/{OWNER_USERNAME}'>{OWNER_NAME}</a>
 
-☠️ <b>{SUPPORT_NAME} :</b> <a href='t.me/{SUPPORT_GRP}'>{SUPPORT_NAME}</a>
+☠️ <b>Support Group :</b> <a href='t.me/{SUPPORT_GRP}'>{SUPPORT_NAME}</a>
 
-☠️ <b>{UPDATES_NAME} :</b> <a href='t.me/{UPDATES_CH}'>{UPDATES_NAME}</a>
+☠️ <b>Updates Channel :</b> <a href='t.me/{UPDATES_CH}'>{UPDATES_NAME}</a>
 
 ☠️ <b>Source Code :</b> <a href='https://github.com/fahrial2310/IMGEditor-TeleBot-IDN'>Source Code</a>
 """
